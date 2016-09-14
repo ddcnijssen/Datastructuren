27873161205b8ed7af27eb14a4da6b3eb6c18b97
ddcnijssen <desleynijssen@outlook.com>
2016-09-14 15:39:37 +0200

    Add files via upload

--- array.h
+++ array.h
@@ -2,37 +2,58 @@
 * klassenaam: beschrijving van klasse/programma
 * @author Nijssen, Desley (s1474146)
 * @author Slotegraaf, Minke (s1430793)
-* @file [Stapel].cc
-* @date datum laatste wijziging
+* @file array.h
+* @date 14 september 2016
 **/
 
-class array{
-   public:
-      void create();
-      bool isEmpty();
-      void clear();
-      bool push(newItem);
-      bool pop();
-      bool top(topItem);
+template <class T> class array{
+	public:
+		void create();
+		bool isEmpty();
+		void clear();
+		bool push(T newItem);
+		bool pop();
+		bool top(T topItem);
+		T stapel[];
 }  
+
 int & count = 0;
 void array::create(int & count){
-   for(int i = 0; i < 50; i++){
-      stapel[i] = NULL;
-   }
-   count = 0;
+	T stapel[50];
 }
+
 bool array::isEmpty(){
-   int i = 0;
-   while(stapel[i] == NULL){
-      if(i >= 50) return true;
-      i++;
-   }
-   return false;
+	int i = 0;
+	while(stapel[i] == NULL){
+		if(i = 50) return true;
+		i++;
+	}
+	return false;
 }
+
 void array::clear(){
-   
+	for(int i = 0; i <= 50; i++){
+		stapel[i] = NULL;
+	}
+	count = 0;
 }
-bool array::push(newItem){}
-bool array::pop(){}
-bool array::top(topItem){}
+
+bool array::push(T newItem){
+	if(count >= 50) return false;
+	stapel[count + 1] = newItem;
+	count++;
+	return true;
+}
+
+bool array::pop(){
+	if(count <= 0) return false;
+	stapel[count] = NULL;
+	count--;
+	return true;
+}
+
+bool array::top(){
+	if(count == 0) return false;
+	cout << "Bovenste element van de stapel is: " << stapel[count] << endl;
+	return true;
+}
