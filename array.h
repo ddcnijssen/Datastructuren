7bf747039681e6342afac1f79480e5b4e1d7e97b
MMSlotegraaf <minkeslotegraaf@gmail.com>
2016-09-15 11:13:13 +0200

    Add files via upload

--- array.h
+++ array.h
@@ -3,26 +3,77 @@
 * @author Nijssen, Desley (s1474146)
 * @author Slotegraaf, Minke (s1430793)
 * @file array.h
-* @date 14 september 2016
+* @date 15 september 2016
 **/
 
 template <class T> class array{
 	public:
+		/**
+		* @function create
+		* @abstract maakt een lege stapel
+		* @pre exacte beschrijving preconditie
+		* @post exacte beschrijving postconditie
+		wat is er veranderd na het uitvoeren van de functie?
+		**/
 		void create();
+		/**
+		* @function isEmpty
+		* @abstract bepaalt of de stapel leeg is
+		* @return geeft true als de stapel leeg is en false als de stapel niet leeg is
+		* @pre exacte beschrijving preconditie
+		* @post exacte beschrijving postconditie
+		wat is er veranderd na het uitvoeren van de functie?
+		**/
 		bool isEmpty();
+		/**
+		* @function clear
+		* @abstract maakt de stapel leeg
+		* @pre exacte beschrijving preconditie
+		* @post exacte beschrijving postconditie
+		wat is er veranderd na het uitvoeren van de functie?
+		**/
 		void clear();
+		/**
+		* @function push
+		* @abstract voegt een newItem toe op de top van de stapel, en geeft terug of de opdracht gelukt is
+		* @param het ding dat aan de stapel wordt toegevoegd
+		* @return geeft true als newItem is toegevoegd aan de stapel
+		* @pre exacte beschrijving preconditie
+		* @post exacte beschrijving postconditie
+		wat is er veranderd na het uitvoeren van de functie?
+		**/
 		bool push(T newItem);
+		/**
+		* @function pop
+		* @abstract verwijdert het bovenste element van de stapel, en geeft terug of de opdracht gelukt is
+		* @return geeft true als het bovenste element van de stapel is verwijderd
+		* @pre exacte beschrijving preconditie
+		* @post exacte beschrijving postconditie
+		wat is er veranderd na het uitvoeren van de functie?
+		**/
 		bool pop();
-		bool top(T topItem);
-		T stapel[];
+		/**
+		* @function top
+		* @abstract geeft het bovenste stapelelement, zonder dit te verwijderen, en geeft terug of de opdracht gelukt is
+		* @return geeft true als het bovenste stapel element is gegeven
+		* @pre exacte beschrijving preconditie
+		* @post exacte beschrijving postconditie
+		wat is er veranderd na het uitvoeren van de functie?
+		**/
+		bool top();
+		T stapel[50];
+
+	private:
+		int & count;
 }  
 
-int & count = 0;
-void array::create(int & count){
-	T stapel[50];
+template <class T>
+array<T>::array(){
+	count = 0;
 }
 
-bool array::isEmpty(){
+template <class T>
+bool array<T>::isEmpty(){
 	int i = 0;
 	while(stapel[i] == NULL){
 		if(i = 50) return true;
@@ -31,28 +82,32 @@ bool array::isEmpty(){
 	return false;
 }
 
-void array::clear(){
+template <class T>
+void array<T>::clear(){
 	for(int i = 0; i <= 50; i++){
 		stapel[i] = NULL;
 	}
 	count = 0;
 }
 
-bool array::push(T newItem){
+template <class T>
+bool array<T>::push(T newItem){
 	if(count >= 50) return false;
 	stapel[count + 1] = newItem;
 	count++;
 	return true;
 }
 
-bool array::pop(){
+template <class T>
+bool array<T>::pop(){
 	if(count <= 0) return false;
 	stapel[count] = NULL;
 	count--;
 	return true;
 }
 
-bool array::top(){
+template <class T>
+bool array<T>::top(){
 	if(count == 0) return false;
 	cout << "Bovenste element van de stapel is: " << stapel[count] << endl;
 	return true;
--- pointers.h
+++ pointers.h
@@ -3,16 +3,70 @@
 * @author Nijssen, Desley (s1474146)
 * @author Slotegraaf, Minke (s1430793)
 * @file pointers.h
-* @date 14 september 2016
+* @date 15 september 2016
 **/
 
-class pointers{
-   public:
-      void create();
-      bool isEmpty();
-      void clear();
-      bool push(newItem);
-      bool pop();
-      bool top(topItem);
-}  
+template <class T> class vakje{
+	public:
+		T info;
+		vakje* next;
+}
 
+template <class T> class pointers{
+	public:
+		void create();
+		bool isEmpty();
+		void clear();
+		bool push(newItem);
+		bool pop();
+		bool top(topItem);
+
+	private:
+		int & count;
+		vakje* item;
+}
+
+template <class T>
+pointers<T>::pointers(){
+	count = 0;
+}
+
+template <class T>
+bool pointers<T>::isEmpty(vakje* & item){
+	if(count = 0) return false;
+	return false;
+}
+
+template <class T>
+void pointers<T>::clear(vakje* & item){
+	vakje* hulp;
+	while(count > 0){
+		hulp = item;
+		item = item->next;
+		delete hulp;
+	}
+}
+
+template <class T>
+bool pointers<T>::push(T newItem){
+	vakje* hulp;
+	hulp = new vakje;
+	hulp->info = newItem;
+	hulp->next = item;
+	item = hulp;
+	count++;
+}
+
+template <class T>
+bool pointers<T>::pop(){
+	vakje*hulp;
+	
+	count--;
+}
+
+template <class T>
+bool pointers<T>::top(topItem){
+	while(item != NULL){
+
+	}
+}
